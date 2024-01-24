24b73a5895323c36f83804c7a2210c875445a4a1
Nello Rizzo <nellorizzo@live.it>
2024-01-24 11:54:01 +0100

    Implementazione della persistenza su file di testo

--- rubrica.py
+++ rubrica.py
@@ -11,7 +11,7 @@ def menu() -> int:
     print("1. Aggiungi")
     print("2. Elenca")
     print("0. Esci")
-    
+
     r = -1
     while r != 0 and r != 1 and r != 2:
         try:
@@ -20,27 +20,52 @@ def menu() -> int:
             r = -1
     return r
 
+
 # l'utente potrà aggiungere numeri alla rubrica
 # quindi ci serve una rubrica!
 rubrica = []
 
 # l'aggiunta dei numeri dovrà prevedere la digitazione del nome
 # e del numero di telefono associato
-def inserisci() :
-    nome = input("Nome: ") # chiede il nome all'utente
-    telefono = input("Numero di telefono: ") # chiede il numero di telefono
-    return nome, telefono # restituisce la tupla (nome, numero)
+
+
+def inserisci():
+    nome = input("Nome: ")  # chiede il nome all'utente
+    telefono = input("Numero di telefono: ")  # chiede il numero di telefono
+    return nome, telefono  # restituisce la tupla (nome, numero)
 
 # la visualizzazione dovrà prevedere la stampa della rubrica
+
+
 def visualizza():
-    print(rubrica)
+    for i in rubrica:
+        print("Nome:", i["nome"], "Telefono", i["telefono"])
+
+
+# implementazione della persistenza su file
+file_rubrica = "./rubrica.txt"
+
+# salva le informazioni su un file di testo
+def salva(nome_file: str):
+    with open(nome_file, "w") as f:
+        f.writelines([e["nome"] + ';' + e["telefono"] + '\n' for e in rubrica])
+
+#legge le informazioni da un file di testo
+def leggi(nome_file:str):
+    with open(nome_file, "r") as f:
+        lines = f.readlines() # legge le righe per intero
+    elab = [l.strip().split(';') for l in lines] # divide ogni riga in base alla presenza di un carattere ;
+    #      trasforma le liste ottenute in precedenza creando per ogni riga un dict
+    return [{'nome': l[0], 'telefono': l[1]} for l in elab]
     
+rubrica = leggi(file_rubrica)
 scelta = -1
 while scelta != 0:
     scelta = menu()
     if scelta == 1:
-        inserisci()
+        nome, numero = inserisci()  # qui ottengo ciò che l'utente ha inserito
+        # lo aggiungo alla rubrica
+        rubrica.append({'nome': nome, 'telefono': numero})
+        salva(file_rubrica)
     elif scelta == 2:
         visualizza()
-
-    
